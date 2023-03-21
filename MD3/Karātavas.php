1a3b244686bb37b03ba41ba8fbac5c3bbff98714
Matīss <kmaters@gmail.com>
2023-03-21 15:08:18 +0200

    Hangman game - done

--- MD3/Karātavas.php
+++ MD3/Karātavas.php
@@ -1,3 +1,49 @@
 <?php
 
- //būs karātavas
+ $words = [
+   'classroom',
+   'popcorn',
+   'defenestrate',
+   'deforestation',
+   'engineer'
+ ];
+
+ $pickWord = array_rand($words, 1);
+ $pickedWord = $words[$pickWord];
+ $playWordSplit = str_split($pickedWord);
+ $playWord = str_split((str_repeat('_', count($playWordSplit))));
+
+ $guesses = 0;
+ $maxGuesses = 3;
+
+ echo 'Lets play *HANGMAN*';
+ echo PHP_EOL;
+ echo '**********************';
+ echo PHP_EOL;
+
+ while($guesses < $maxGuesses) {
+
+   echo implode($playWord);
+   echo PHP_EOL;
+
+   $playerSelection = readline('Please pick a letter:');
+   $playerSelectionIndex = array_keys($playWordSplit, $playerSelection);
+   if ($playerSelectionIndex[0] > -1) {
+     foreach ($playerSelectionIndex as $index) {
+       $playWord[$index] = $playerSelection;
+     }
+   } else {
+     $guesses++;
+   }
+
+   if ($playWord == $playWordSplit) {
+     echo "Correct, your word is - $pickedWord";
+     echo PHP_EOL;
+     echo 'You Win';
+     exit;
+   }
+ }
+  echo "Your word was - $pickWord";
+  echo PHP_EOL;
+  echo 'You Lose';
+
