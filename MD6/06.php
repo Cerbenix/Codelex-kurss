8487302b7a8ab730b2e5bfc25fb34125c986a1d5
Matīss <kmaters@gmail.com>
2023-04-11 19:48:58 +0300

    Changed function naming

--- MD6/06.php
+++ MD6/06.php
@@ -15,19 +15,19 @@ $surveyed = 12467;
 $purchased_energy_drinks = 0.14;
 $prefer_citrus_drinks = 0.64;
 
-function calculate_energy_drinkers(int $numberSurveyed, float $percent): float
+function calculateEnergyDrinkers(int $numberSurveyed, float $percent): float
 {
   return round($numberSurveyed * $percent);
 }
 
-function calculate_prefer_citrus(int $numberSurveyed, float $percent, float $percentCitrusPreference): float
+function calculatePreferCitrus(int $numberSurveyed, float $percent, float $percentCitrusPreference): float
 {
   return round($numberSurveyed * $percent * $percentCitrusPreference);
 }
 
 
 echo "Total number of people surveyed " . $surveyed . PHP_EOL;
-echo "Approximately " . calculate_energy_drinkers($surveyed, $purchased_energy_drinks) .
+echo "Approximately " . calculateEnergyDrinkers($surveyed, $purchased_energy_drinks) .
   " bought at least one energy drink." . PHP_EOL;
-echo calculate_prefer_citrus($surveyed, $purchased_energy_drinks, $prefer_citrus_drinks) . " of those " .
+echo calculatePreferCitrus($surveyed, $purchased_energy_drinks, $prefer_citrus_drinks) . " of those " .
   "prefer citrus flavored energy drinks.";
